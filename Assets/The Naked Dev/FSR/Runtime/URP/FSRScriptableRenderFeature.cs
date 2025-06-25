5f97749a071eeb7b9c8b86d1bac377e3e475d102
Andy DiLallo <39313955+adilallo@users.noreply.github.com>
2025-06-25 12:12:37 -0600

    Added FSR upscaling

---
+++ Assets/The Naked Dev/FSR/Runtime/URP/FSRScriptableRenderFeature.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace TND.FSR
+{
+    //Not allowed to be in a namespace
+    public class FSRScriptableRenderFeature : ScriptableRendererFeature
+    {
+        [HideInInspector]
+        public bool IsEnabled = false;
+        private bool usingRenderGraph = false;
+
+        private FSR3_URP m_upscaler;
+
+        private FSRBufferPass _bufferPass;
+        private FSRRenderPass _renderPass;
+        private FSROpaqueOnlyPass _opaqueBufferPass;
+        private FSRTransparentPass _transparentBufferPass;
+
+        private CameraData cameraData;
+
+        public override void Create()
+        {
+            name = "FSRRenderFeature";
+            SetupPasses();
+        }
+
+        public void OnSetReference(FSR3_URP _upscaler)
+        {
+            m_upscaler = _upscaler;
+            SetupPasses();
+        }
+
+        private void SetupPasses()
+        {
+#if UNITY_2023_3_OR_NEWER
+            var renderGraphSettings = GraphicsSettings.GetRenderPipelineSettings<RenderGraphSettings>();
+            usingRenderGraph = !renderGraphSettings.enableRenderCompatibilityMode;
+#endif
+
+            if (!usingRenderGraph)
+            {
+                _bufferPass = new FSRBufferPass(m_upscaler);
+                _bufferPass.ConfigureInput(ScriptableRenderPassInput.Depth | ScriptableRenderPassInput.Motion);
+            }
+
+            _renderPass = new FSRRenderPass(m_upscaler, usingRenderGraph);
+            _renderPass.ConfigureInput(ScriptableRenderPassInput.Depth | ScriptableRenderPassInput.Motion);
+
+            _opaqueBufferPass = new FSROpaqueOnlyPass(m_upscaler);
+            _transparentBufferPass = new FSRTransparentPass(m_upscaler);
+        }
+
+        public void OnDispose()
+        {
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+#endif
+            if (!IsEnabled || m_upscaler == null)
+            {
+                return;
+            }
+
+            cameraData = renderingData.cameraData;
+            if (cameraData.camera != m_upscaler.m_mainCamera)
+            {
+                return;
+            }
+            if (!cameraData.resolveFinalTarget)
+            {
+                return;
+            }
+
+            m_upscaler.m_autoHDR = cameraData.isHdrEnabled;
+
+            // Here you can queue up multiple passes after each other.
+            if (!usingRenderGraph)
+            {
+                renderer.EnqueuePass(_bufferPass);
+            }
+
+            renderer.EnqueuePass(_renderPass);
+            if (m_upscaler.generateReactiveMask)
+            {
+                renderer.EnqueuePass(_opaqueBufferPass);
+                renderer.EnqueuePass(_transparentBufferPass);
+            }
+        }
+    }
+}
